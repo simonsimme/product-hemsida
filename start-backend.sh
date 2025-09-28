29b3c2199d79b4c4d4951440a32a3860ee05a11a
simonsimme <simonsimme@hotmail.se>
2025-09-28 14:01:35 +0200

    CI, qucik starts

---
+++ start-backend.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Enable debugging
+set -x
+
+
+echo "Navigating to the backend directory..."
+cd /home/simon/projects/devops/kruso\ hemsida/product-hemsida/backend/backend-java || { echo "Failed to navigate to backend directory"; exit 1; }
+
+docker-compose up -d
+
+./mvnw spring-boot:run
